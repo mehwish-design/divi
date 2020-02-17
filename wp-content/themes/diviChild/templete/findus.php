5e647a1c98ed74d0cfd42667c8ca957be916a60b
fatima-777 <fzehra777@gmail.com>
2020-02-17 13:03:43 +0500

    stripe

---
+++ wp-content/themes/diviChild/templete/findus.php
@@ -0,0 +1,4 @@
+<?php
+/*Template Name: Custom Find Us */ 
+?>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3619.741442599134!2d67.05837025030544!3d24.872678883969968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e923adec49d%3A0xcc2dbd936f1c5627!2sTariq%20Rd%2C%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1581925982124!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
